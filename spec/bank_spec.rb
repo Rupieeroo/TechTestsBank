68140aeb613cf6014e623c9fab1c6a1cb50c2380
Rupert Adams <RupertNCAdams@aol.com>
2018-12-03 11:11:31 +0000

    wrote first test to pass

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -5,4 +5,12 @@ describe 'Test working' do
     bank = Bank.new
     expect(bank.hello).to eq('Hello World')
   end
+
+  describe 'Depositing money' do
+    it 'Deposits money into a users account' do
+      bank = Bank.new
+      bank.deposit(1000)
+      expect(bank.account).to eq(1000)
+    end
+  end
 end
