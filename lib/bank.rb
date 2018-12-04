7e388bf307246ed16671ea781b6755aafc484e6b
Rupert Adams <RupertNCAdams@aol.com>
2018-12-04 14:45:09 +0000

    made the hard coding better, needs work

--- lib/bank.rb
+++ lib/bank.rb
@@ -32,7 +32,7 @@ class Bank
   end
 
   def statement
-    state =  "date || credit || debit || balance\n      #{@transactions[-1][:date]} || || #{@transactions[-1][:debit]}.00 || #{@account}.00\n      #{@transactions[-2][:date]} || #{@transactions[-2][:credit]}.00 || || #{@account + @transactions[-1][:debit]}.00\n      #{@transactions[-3][:date]} || #{@transactions[-3][:credit]}.00 || || #{history[-3]}.00"
+    state =  "date || credit || debit || balance\n      #{@transactions[-1][:date]} || || #{@transactions[-1][:debit]}.00 || #{@account}.00\n      #{@transactions[-2][:date]} || #{@transactions[-2][:credit]}.00 || || #{@account + @transactions[-1][:debit]}.00\n      #{@transactions[-3][:date]} || #{@transactions[-3][:credit]}.00 || || #{@account + @transactions[-1][:debit] - @transactions[-2][:credit] }.00"
     return state
   end
 end
