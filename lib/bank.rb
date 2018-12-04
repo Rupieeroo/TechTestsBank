2b74acf936c9873190f76d614442512eff749332
Rupert Adams <RupertNCAdams@aol.com>
2018-12-04 14:31:50 +0000

    hard coded statement method for testing

--- lib/bank.rb
+++ lib/bank.rb
@@ -32,7 +32,7 @@ class Bank
   end
 
   def statement
-    state =  "date || credit || debit || balance\n      14/01/2012 || || 500.00 || 2500.00\n      13/01/2012 || 2000.00 || || 3000.00\n      10/01/2012 || 1000.00 || || 1000.00"
+    state =  "date || credit || debit || balance\n      #{@transactions[-1][:date]} || || #{@transactions[-1][:debit]}.00 || #{@account}.00\n      #{@transactions[-2][:date]} || #{@transactions[-2][:credit]}.00 || || #{@account + @transactions[-1][:debit]}.00\n      #{@transactions[-3][:date]} || #{@transactions[-3][:credit]}.00 || || #{history[-3]}.00"
     return state
   end
 end
