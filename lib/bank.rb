f5add8d93d07379cf27fb072953e110008827272
Rupert Adams <RupertNCAdams@aol.com>
2018-12-05 12:45:13 +0000

    fixed debugging user criteria for statement

--- lib/bank.rb
+++ lib/bank.rb
@@ -17,14 +17,14 @@ class Bank
     @account += money
     @history.push(money)
     @dates.push(date)
-    @transactions << { method: 'deposit', credit: money, date: date }
+    @transactions << { method: 'deposit', credit: money, date: date, balance: @account }
   end
 
   def withdraw(money, date)
     @account -= money
     @history.push(money)
     @dates.push(date)
-    @transactions << { method: 'withdraw', debit: money, date: date }
+    @transactions << { method: 'withdraw', debit: money, date: date, balance: @account }
   end
 
   def transaction_method
@@ -34,13 +34,12 @@ class Bank
   def statement
     # state =  "date || credit || debit || balance\n      #{@transactions[-1][:date]} || || #{@transactions[-1][:debit]}.00 || #{@account}.00\n      #{@transactions[-2][:date]} || #{@transactions[-2][:credit]}.00 || || #{@account + @transactions[-1][:debit]}.00\n      #{@transactions[-3][:date]} || #{@transactions[-3][:credit]}.00 || || #{@account + @transactions[-1][:debit] - @transactions[-2][:credit] }.00"
     # return state
-    @tally = 0
     message_statement = "date || credit || debit || balance\n"
-    @transactions.each do |transaction|
+    @transactions.reverse.each do |transaction|
       if transaction[:credit] == nil
-        message_statement += "#{transaction[:date]} || || #{transaction[:debit]}.00 || #{(@tally -= transaction[:debit])}.00\n"
+        message_statement += "#{transaction[:date]} || || #{transaction[:debit]}.00 || #{transaction[:balance]}.00\n"
       else
-        message_statement += "#{transaction[:date]} || #{transaction[:credit]}.00 || || #{(@tally += transaction[:credit])}.00\n"
+        message_statement += "#{transaction[:date]} || #{transaction[:credit]}.00 || || #{transaction[:balance]}.00\n"
       end
     end
     return message_statement
