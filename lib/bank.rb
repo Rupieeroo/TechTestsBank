7f697ab04ab3fadf09e4e64cf01d909bf9004538
Rupert Adams <RupertNCAdams@aol.com>
2018-12-05 11:14:44 +0000

    Statement method now prints perfectly, in the wrong decending order

--- lib/bank.rb
+++ lib/bank.rb
@@ -34,12 +34,13 @@ class Bank
   def statement
     # state =  "date || credit || debit || balance\n      #{@transactions[-1][:date]} || || #{@transactions[-1][:debit]}.00 || #{@account}.00\n      #{@transactions[-2][:date]} || #{@transactions[-2][:credit]}.00 || || #{@account + @transactions[-1][:debit]}.00\n      #{@transactions[-3][:date]} || #{@transactions[-3][:credit]}.00 || || #{@account + @transactions[-1][:debit] - @transactions[-2][:credit] }.00"
     # return state
+    @tally = 0
     message_statement = "date || credit || debit || balance\n"
-    @transactions.reverse.each do |transaction|
+    @transactions.each do |transaction|
       if transaction[:credit] == nil
-        message_statement += "#{transaction[:date]} || || #{transaction[:debit]}.00 || #{@account}.00\n"
+        message_statement += "#{transaction[:date]} || || #{transaction[:debit]}.00 || #{(@tally -= transaction[:debit])}.00\n"
       else
-        message_statement += "#{transaction[:date]} || #{transaction[:credit]}.00 || || #{@account}.00\n"
+        message_statement += "#{transaction[:date]} || #{transaction[:credit]}.00 || || #{(@tally += transaction[:credit])}.00\n"
       end
     end
     return message_statement
